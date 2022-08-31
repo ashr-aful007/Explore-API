6c89945d1dfd339ba7b29e44dd6e817ae5835499
ashr-aful007 <ashraful99980@gmail.com>
2022-08-31 15:46:35 +0600

    add slice and toggle function

--- recap-phone-hunter/js/phone.js
+++ recap-phone-hunter/js/phone.js
@@ -6,11 +6,20 @@ const dataload = async (searchPhone) =>{
 }
 
 const displayData = phones =>{
-
+    const noPhoneAlrt = document.getElementById('no-phoneAlrt"');
+  // if(phones.langth === 0){
+  //   noPhoneAlrt.classList.remove('d-none')
+  // }
+  // else{
+  //   noPhoneAlrt.classList.add('d-none')
+  // }
+  
     const phoneContaner = document.getElementById('phone-Contaner');
+    phoneContaner.innerText = '';
 
-    phones.forEach(phone =>{
-     
+    phones = phones.slice(0, 10);
+
+    phones.forEach(phone =>{      
        const phoneDiv = document.createElement('div');
        phoneDiv.classList.add('col');
        phoneDiv.innerHTML = `      
@@ -25,24 +34,43 @@ const displayData = phones =>{
        phoneContaner.appendChild(phoneDiv)
 
     })
+    toggleSpiner(false)
 }
 
-const searchInputText = () =>{
-     const inputFildText = document.getElementById('input-fildText');
+
+const searchInputText = () =>{     
+     const inputFildText = document.getElementById('input-fildText');    
      const searchText = inputFildText.value;
      dataload(searchText)
+     inputFildText.value = '';
      
   }
 
 
 document.getElementById('search-input').addEventListener('click', function(){
+  toggleSpiner(true)
      searchInputText()
 
 })
 
+document.getElementById('input-fildText').addEventListener('keypress', function(event){
+  
+   if(event.key === 'Enter'){
+    searchInputText()
+    toggleSpiner(true) //start loader
+   }
+})
 
-
-
+//loader spinner 
+const toggleSpiner = isloading =>{
+  const loaderSpinr = document.getElementById('loader');
+  if(isloading == true){
+    loaderSpinr.classList.remove('d-none')
+  }
+  else{
+    loaderSpinr.classList.add('d-none')
+  }
+}
 
 
 dataload()
