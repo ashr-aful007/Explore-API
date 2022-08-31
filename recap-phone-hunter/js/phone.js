7bfac5d2631eda3ceed6ff6ac89f74949f8b7940
ashr-aful007 <ashraful99980@gmail.com>
2022-08-31 12:51:48 +0600

    add phone card

---
+++ recap-phone-hunter/js/phone.js
@@ -0,0 +1,39 @@
+const dataload = async () =>{
+     const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+     const res = await fetch(url);
+     const data = await res.json();
+     displayData(data.data)
+}
+
+const displayData = phones =>{
+     console.log(phones)
+    const phoneContaner = document.getElementById('phone-Contaner');
+
+    phones.forEach(phone =>{
+     
+       const phoneDiv = document.createElement('div');
+       phoneDiv.classList.add('col');
+       phoneDiv.innerHTML = `      
+       <div class="card">
+         <img src="${phone.image}" class="card-img-top" alt="...">
+         <div class="card-body">
+           <h5 class="card-title">${phone.phone_name}</h5>
+           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+         </div>
+       </div>    
+       `;
+       phoneContaner.appendChild(phoneDiv)
+
+    })
+}
+
+
+
+
+
+
+
+
+
+
+dataload()
